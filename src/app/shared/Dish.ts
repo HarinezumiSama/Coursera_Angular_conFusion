61bbf465d165ad2107be176bd766d377d9c11292
Vitalii Maklai <vitalii.maklai@gmail.com>
2018-12-25 00:17:50 -0500

    Components Part 1

---
+++ src/app/shared/Dish.ts
@@ -0,0 +1,11 @@
+﻿export class Dish
+{
+    public id: string;
+    public name: string;
+    public image: string;
+    public category: string;
+    public featured: boolean;
+    public label: string;
+    public price: string;
+    public description: string;
+}
