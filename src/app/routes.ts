44bc46ad011d58e13c246b23d9763c3177b6f12a
Vitalii Maklai <vitalii.maklai@gmail.com>
2018-12-26 15:34:51 -0500

    Fix/improvement in the route list

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,13 +2,14 @@
 
 import { MenuComponent } from './components/menu/menu.component';;
 import { HomeComponent } from './components/home/home.component';
+
 ////import { AboutComponent } from './components/about/about.component';
 ////import { ContactComponent } from './components/contact/contact.component';
 ////import { DishdetailComponent } from './components/dishdetail/dishdetail.component';
 
 export const ROUTES: Routes =
 [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'menu', component: MenuComponent }
+    { path: 'menu', component: MenuComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
