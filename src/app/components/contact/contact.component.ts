9dbbcf33828e77200dbf296cc181e3f5fc16a037
Vitalii Maklai <vitalii.maklai@gmail.com>
2018-12-30 21:19:32 -0500

    Reactive Forms Part 3

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -10,12 +10,49 @@ import { Feedback, CONTACT_TYPES } from '../../shared/feedback';
     })
 export class ContactComponent implements OnInit
 {
+    private static minNameLength = 2;
+    private static maxNameLength = 40;
+
     public feedbackForm: FormGroup;
     public feedback: Feedback;
     public contactTypes = CONTACT_TYPES;
 
     @ViewChild('fform')
     public feedbackFormDirective;
+
+    private formErrors =
+    {
+        'firstName': '',
+        'lastName': '',
+        'telNum': '',
+        'email': ''
+    };
+
+    private validationMessages =
+    {
+        'firstName':
+        {
+            'required': 'First Name is required.',
+            'minlength': `First Name must be at least ${ContactComponent.minNameLength} characters long.`,
+            'maxlength': `First Name cannot be more than ${ContactComponent.maxNameLength} characters long.`
+        },
+        'lastName':
+        {
+            'required': 'Last Name is required.',
+            'minlength': `Last Name must be at least ${ContactComponent.minNameLength} characters long.`,
+            'maxlength': `Last Name cannot be more than ${ContactComponent.maxNameLength} characters long.`
+        },
+        'telNum':
+        {
+            'required': 'Telephone number is required.',
+            'pattern': 'Telephone number may only contain digits.'
+        },
+        'email':
+        {
+            'required': 'Email is required.',
+            'email': 'Email format is invalid.'
+        },
+    };
 
     constructor(private readonly formBuilder: FormBuilder)
     {
@@ -30,14 +67,32 @@ export class ContactComponent implements OnInit
     {
         this.feedbackForm = this.formBuilder.group(
             {
-                firstName: ['', Validators.required],
-                lastName: ['', Validators.required],
-                telNum: ['', Validators.required],
-                email: ['', Validators.required],
+                firstName: [
+                    '',
+                    [
+                        Validators.required,
+                        Validators.minLength(ContactComponent.minNameLength),
+                        Validators.maxLength(ContactComponent.maxNameLength)
+                    ]
+                ],
+                lastName: [
+                    '',
+                    [
+                        Validators.required,
+                        Validators.minLength(ContactComponent.minNameLength),
+                        Validators.maxLength(ContactComponent.maxNameLength)
+                    ]
+                ],
+                telNum: ['', [Validators.required, Validators.pattern]],
+                email: ['', [Validators.required, Validators.email]],
                 agree: false,
                 contactType: 'None',
                 message: ''
             });
+
+        this.feedbackForm.valueChanges.subscribe(data => this.onValueChanged(data));
+
+        this.onValueChanged(); // Reset form validation messages
     }
 
     public onSubmit()
@@ -57,5 +112,40 @@ export class ContactComponent implements OnInit
             });
 
         this.feedbackFormDirective.resetForm();
+    }
+
+    private onValueChanged(data?: any): void
+    {
+        if (this.feedbackForm == null)
+        {
+            return;
+        }
+
+        const form = this.feedbackForm;
+
+        for (const field in this.formErrors)
+        {
+            if (!this.formErrors.hasOwnProperty(field))
+            {
+                continue;
+            }
+
+            this.formErrors[field] = '';
+
+            const control = form.get(field);
+            if (control == null || !control.dirty || !control.invalid)
+            {
+                continue;
+            }
+
+            const messages = this.validationMessages[field];
+            for (const key in control.errors)
+            {
+                if (control.errors.hasOwnProperty(key))
+                {
+                    this.formErrors[field] += messages[key] + ' ';
+                }
+            }
+        }
     }
 }
